c2ee65c97b32ebefe54eb7557751196b5ec4721e
Kavita Gupta <124678732+kika8919@users.noreply.github.com>
2023-06-16 21:22:06 +0530

    Update home.route.js

--- routes/home.route.js
+++ routes/home.route.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const categoryController = require("../controllers/categoryController");
 const verifyJWT = require("../middleware/verifyJWT");
 
-
 // Authentication
 router.get("/category", categoryController.getCategories);
 
-
+router.get("/category/:categoryId", categoryController.getLocationsByCategory);
 
 // Registration
 // router.post("/register", userController.registerUser);
-
 
 module.exports = router;
