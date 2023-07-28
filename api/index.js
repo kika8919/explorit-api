636d454cfa1553e14feaad62b03deaed0db2adfd
Kavita Gupta <124678732+kika8919@users.noreply.github.com>
2023-07-28 19:36:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,32 +1,3 @@
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 4000;
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const corsOptions = require("../config/corsOptions");
-const connectDB = require("../config/dbConnect");
-const mongoose = require("mongoose");
-
-connectDB();
-
-app.use(cors(corsOptions));
-app.use(express.json()); // middleware to parse json
-app.use(cookieParser());
-
-// user routes - for /api/users and /api/user
-app.use("/api", require("../routes"));
-
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-});
+const app = require("../app");
 
 module.exports = app;
