36a2d45ee2bbff3c5cf1583d36d7253da77b64d3
Kavita Gupta <124678732+kika8919@users.noreply.github.com>
2023-07-28 18:55:34 +0530

    Update allowedOrigins.js for vercel webapp
    explorit-webapp.vercel.app

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:4200"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:4200","https://explorit-webapp.vercel.app"];
 
 module.exports = allowedOrigins;
